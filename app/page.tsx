6084d8d1311ae2eece8b56b81c7c707846dd6d19
induwarafernando <induwarafernando9@gmail.com>
2024-11-06 20:24:24 +0530

    final update - was unable to complete

--- app/page.tsx
+++ app/page.tsx
@@ -36,17 +36,18 @@ export default function Home() {
   style={{
     backgroundImage: 'url(https://s3-alpha-sig.figma.com/img/de5a/d4b1/280d764a1fe47880cc185b53132efe7a?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=CT57LDQUCu78OyHJLlx8frfKP91~wQHWRZ9YDhmyHEbtgFfTEJxb5h4UoGJcK1cDTcqKSmgUEe7Etve5SMLIyaelh3PAHUZ0VxWYL6H0kOjdnQcwGaBfulDh~NByJ7MAlYW8YiyHQMGvYY-Uesvg9kVMLW0uF7d4lfMMhrpZScNp4g0yLZtBHxwR2feXKwkLBw3tiPAa51loHA4LPRxmb~xhplpKa4sTFxZ3Fy0zCchh9pskrbDjX8Nd6nIIcZp7f2z7HW79AqTjiTxT4d4JgMXaKkgSqoA3pU4TblLL80yB6J9WYIjrqFoLUFPkhepxLpVVvcvqL00SwqZrRfiVHg__)',
     backgroundSize: 'cover',
-    backgroundPosition: 'center'
+    backgroundPosition: 'center',
+    height: '78vh'
   }}
 >
   {/* Black overlay with opacity */}
-  <div className="absolute inset-0 bg-black opacity-50 rounded-lg"></div>
+  <div className="absolute inset-0 bg-black opacity-60 rounded-lg"></div>
 
   {/* Content with left alignment */}
   <div className="relative z-10 max-w-3xl mx-2 px-8">
-    <h1 className="text-4xl font-bold mb-4">Fuel Your Body With Healthy Choices</h1>
+    <h1 className="text-6xl font-bold mb-4">Fuel Your Body With Healthy Choices</h1>
     <p className="text-lg mb-8">Explore our wide range of healthy, delicious meals.</p>
-    <button className="bg-gradient-to-r from-green-400 to-green-600 text-white px-6 py-2 rounded-full text-lg hover:from-green-500 hover:to-green-700">
+    <button className="bg-gradient-to-r from-green-400 to-green-600 text-white px-6 py-2 mt-48 rounded-full text-lg hover:from-green-500 hover:to-green-700">
   Explore the menu
 </button>
 
@@ -73,7 +74,7 @@ export default function Home() {
   <div className="relative z-10 flex flex-col justify-between h-full p-4">
     {/* Title at the top */}
     <div className="text-left">
-      <h2 className="text-3xl font-bold text-white">
+      <h2 className="text-4xl font-bold text-white">
         Bursting with <br />
         <span className="text-green-200">Freshness</span>
       </h2>
@@ -184,6 +185,194 @@ export default function Home() {
 </div>
 
 </section>
+
+<section className="py-8">
+<div className="relative rounded-lg overflow-hidden  mx-auto shadow-lg">
+  {/* <!-- Background Image --> */}
+  <img 
+    src="https://s3-alpha-sig.figma.com/img/e619/be9c/42834f9f4bb908a5883760316783f3d2?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=KSsnaVu8K10lhasxeRAG1G1NhI7P8COnKjXAT2pGhVbJso0oOTqmEq-N4yY0pO2FSfaFYr4dVMAc2zrM2Ag1SpfLqpw-GCODMaGaC7Gbw~gMjzcrDAdW7553bVzaIwaYUePuZwYt60WNyeaiIBN5bbu6ouh4Zfn2MTLKuFWWX21nb7M3XaVIR5c~M31TRVdH2gxpQbqxb6B0D~SN4uJSZ6L8vy5RIju2kcayI8FTg4E-HlTV9ZEG1SHOJcBMJiMOVvAy~bfOqTZgY-T9zQofO-M7WQnLFVBb8MCWUhQ-mdpmAnt-MHOo~DHlQhSnxNBZ9PyKV7dpDE09lHw2IIdf-g__" 
+    alt="Superfruit Bowl" 
+    className="w-full object-cover  h-[500px]"
+    />
+  
+  {/* <!-- Overlay with Gradient and Text --> */}
+  <div className="absolute inset-0 bg-gradient-to-r from-black/70 to-transparent flex items-top">
+    <div className="p-8 text-white">
+      <h2 className="text-5xl font-bold">Your Wellness. <span className="text-green-300">Our Passion.</span></h2>
+      
+      <div className="bg-white bg-opacity-30 backdrop-blur-md p-6 mt-48 rounded-lg text-left text-white">  
+          <p className="text-2xl text-left font-semi-bold mb-4">Golden Superfruit <br /> Delight Bowl</p>
+    <button className="mt-4 px-6 py-2  bg-opacity-60 rounded-full text-white font-semibold hover:bg-opacity-80 transition border-2 border-white">
+      Order Now →
+    </button>
+  </div>
+
+    </div>
+  </div>
+</div>
+</section>
+
+<section className="flex flex-col items-center p-8">
+    {/* Section Title */}
+    <h2 className="text-center text-3xl font-bold text-gray-800">
+        Best Categories <br /> 
+        <span className="font-light">We Have</span>
+    </h2>
+    
+    {/* Tags */}
+    <div className="flex justify-center gap-4 mt-4 mb-8">
+        <span className="px-4 py-2 bg-green-50 text-gray-700 rounded-full text-sm">Low-Sugar</span>
+        <span className="px-4 py-2 bg-green-50 text-gray-700 rounded-full text-sm">Protein</span>
+        <span className="px-4 py-2 bg-green-500 text-white rounded-full text-sm">Classic</span>
+        <span className="px-4 py-2 bg-green-50 text-gray-700 rounded-full text-sm">Green</span>
+    </div>
+
+    {/* Grid of Cards */}
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-5xl">
+        {/* Card 1 */}
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+  {/* Card 1 */}
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+  {/* Card 1 */}
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+  {/* Card 1 */}
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+  {/* Card 1 */}
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+  {/* Card 1 */}
+  <div className="bg-white rounded-lg shadow-lg overflow-hidden transform transition duration-200 hover:scale-105">
+            <div className="p-4">
+                <img src="https://s3-alpha-sig.figma.com/img/1d0f/9638/fc4023d36f8b94e7096ce17b684b149f?Expires=1731283200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=J~tspI6TQ33iUVt~qeG1V1P17SvzXEq0aSMhbak1YLQjV1mll-iURQjYablRimDhaf1vDbRbr8nLcQP2~g8dRhDtG1BERkQt3A426sKmeFrqhJDC3AEicSw71z7XuEN9X5KEQ2RHa4yU32P1r3JrmOd1Bd~q4luvuLLsIOEF~6SV8Ph3cWWGv5ZGVF9HD~hOQuwc5~ODl2aWT9bJ~rIrAnupkBZ3FKkd039BvC3a3PeB9eQEt~67-x5s9x-pfZ2CldDqGdrgzkQG83vQR5ZBwHyenLiy~XXttDeQq9kTFK0pnqy6tSrSfc2Ic0aabJeL6hg26SqokaPIqMdPLRtafA__" 
+                alt="Berry Bliss Bowl" 
+                className="w-full h-96 object-cover rounded-lg" />
+            </div>
+            <div className="flex items-center justify-between bg-green-500 text-white rounded-lg p-4 m-2">
+                <div>
+                    <h3 className="text-xl font-semibold">Berry Bliss Bowl</h3>
+                    <p className="text-gray-200">$9.99</p>
+                </div>
+                <div className="bg-white rounded-full p-2">
+                    <img 
+                        src="https://img.icons8.com/?size=100&id=23175&format=png&color=000000" 
+                        alt="Cart Icon" 
+                        className="w-6 h-6" 
+                    />
+                </div>
+            </div>
+        </div>
+
+
+         </div>
+</section>
+
+
+
+
+  
+        {/* Footer */}
+        <footer className="bg-green-500 text-white my-8 py-8 text-center">
+    <p>&copy; 2021 Fresh Bowl. All rights reserved.</p>
+  </footer>
 
     </div>
   );
